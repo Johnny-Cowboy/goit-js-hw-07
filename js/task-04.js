c4638f646d5fb85097a71c1a7bcbd2c2c358720d
JohnCowboy <dawid.kowalski@protonmail.com>
2023-03-02 08:45:50 +0100

    task-4

--- js/task-04.js
+++ js/task-04.js
@@ -18,11 +18,7 @@ function createBoxes(amount) {
 function destroyBoxes() {
   boxesContainer.innerHTML = "";
 }
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+
 const createButton = document.querySelector("[data-create]");
 createButton.addEventListener("click", () => {
   const amount = document.querySelector('input[type="number"]').value;
